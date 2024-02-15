894c41fca753deac8a8bee1544daad58d354eec4
tm-abby-moreno <abbym@thinkingmachin.es>
2024-02-15 18:14:59 +0800

    Refactor functions and add markdowns

--- src/model_data_prep.py
+++ src/model_data_prep.py
@@ -1,3 +1,63 @@
+import os
+
+import pandas as pd
+
+from src.settings import CLIMATE_VARIABLES_LIST, RAW_DIR
+
+CLIMATE_FILES = os.listdir(RAW_DIR / "climate")
+
+
+def align_climate_var(climate_var, min_year=2013):
+    """
+    This function aggregates the climate variable datasets weekly
+    and then returns one master dataframe for all the variables.
+
+    Args:
+      climate_var: Climate variable.
+      min_year: minimum year to filter the climate dataset to match health data to be used.
+    """
+    raw_df = [
+        filename for filename in CLIMATE_FILES if filename.startswith(climate_var)
+    ][0]
+    var_df = pd.read_csv(RAW_DIR / "climate" / raw_df)
+    var_df["DATE"] = pd.to_datetime(var_df["DATE"])
+    # filter date
+    var_df = var_df[var_df["DATE"].dt.year >= min_year]
+    # add weekly timestamp
+    var_df["start_of_week"] = var_df["DATE"] - pd.to_timedelta(
+        var_df["DATE"].dt.dayofweek, unit="D"
+    )
+
+    # Check if the Monday is from the previous year (December)
+    previous_year_mask = var_df["start_of_week"].dt.year < var_df["DATE"].dt.year
+    # # Adjust the start of the week to the current year
+    var_df.loc[previous_year_mask, "start_of_week"] = pd.to_datetime(
+        var_df[previous_year_mask]["DATE"].dt.year, format="%Y"
+    )
+
+    agg_dict = {
+        "AVG": ("mean", "AVG"),
+        "MIN": ("min", "MIN"),
+        "MAX": ("max", "MAX"),
+        "STD": ("std", "STD"),
+    }
+
+    result_dict = {}
+
+    for agg_function, column_name in agg_dict.values():
+        new_column_name = f"{climate_var}_{column_name}"
+        result = var_df.groupby(["start_of_week", "ADM4_PCODE"])[climate_var].agg(
+            **{new_column_name: agg_function}
+        )
+        result_dict[new_column_name] = result
+
+    # Combine the results into a single DataFrame
+    var_weekly = pd.concat(result_dict.values(), axis=1).reset_index()
+    var_weekly[f"{climate_var}_STD"] = var_weekly[f"{climate_var}_STD"].fillna(0)
+
+    return var_weekly
+
+
 def create_outbreak_summary(tagged_df, target_class):
     """
     Args:
