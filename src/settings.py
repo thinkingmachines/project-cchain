0145d27dddf67863ca272b698b7a93dcbf0810d1
Abby Moreno <109585824+tm-abby-moreno@users.noreply.github.com>
2024-06-10 13:51:41 +0800

    Create sample EDA and consolidate past EDA notebooks into a single one (#14)
    * EDA on exposure variables and incidence
    
    * Refactor plotting for chloropleth
    
    * Consolidate past EDA notebooks
    
    * Place viz utils in py file
    
    * Update root directory
    
    * Remove redundant EDA
    
    * add markdowns
    
    * Remove unnecessary fpaths
    
    ---------
    
    Co-authored-by: tm-jace-peralta <jcp@thinkingmachin.es>

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 # The ROOT_DIR should represent the absolute path of the project root folder
-ROOT_DIR = Path(__file__).absolute().parent.parent
+ROOT_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = ROOT_DIR / "data"
 RAW_DIR = DATA_DIR / "02-raw"
